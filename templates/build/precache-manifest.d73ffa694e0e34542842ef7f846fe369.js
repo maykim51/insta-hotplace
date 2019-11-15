f9cbe1c9d99985e43f705894651d2b33c91cecd2
dudusae <dudusae@gmail.com>
2019-11-15 17:14:43 +0900

    add build folder(react)

---
+++ templates/build/precache-manifest.d73ffa694e0e34542842ef7f846fe369.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c769e8d39b869c7072a040e59710d883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7693784c569ee506955",
+    "url": "/static/css/main.cc5bd78e.chunk.css"
+  },
+  {
+    "revision": "c1d1bcf4fa283f3b0981",
+    "url": "/static/js/2.58b51338.chunk.js"
+  },
+  {
+    "revision": "f7693784c569ee506955",
+    "url": "/static/js/main.888a27d5.chunk.js"
+  },
+  {
+    "revision": "a4d99e990a80c68753fd",
+    "url": "/static/js/runtime-main.e6faa928.js"
+  },
+  {
+    "revision": "e6e31e09dc864bef43c1d2fb08acfad0",
+    "url": "/static/media/JalnanOTF.e6e31e09.otf"
+  },
+  {
+    "revision": "765d91668b9114f8847299207c95b6d3",
+    "url": "/static/media/ico_main@2x.765d9166.png"
+  },
+  {
+    "revision": "d079dd5e2bceeabcd894e528f6c611e0",
+    "url": "/static/media/sample_map.d079dd5e.png"
+  }
+]);
